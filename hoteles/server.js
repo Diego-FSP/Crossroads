1c41d597df65649da32475c1dfa441bfa3127d58
joseph71222 <joseph71222@gmail.com>
2025-10-29 23:05:40 -0300

    listo

--- hoteles/server.js
+++ hoteles/server.js
@@ -22,7 +22,6 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 const db = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || 'root',
   password: process.env.DB_PASS || 'Jhoselin712.',
   database: process.env.DB_NAME || 'hotelesBA'
 });
