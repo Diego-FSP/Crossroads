1cdb5756ebbceb3d1962c30b7c448d106d731f08
joseph71222 <joseph71222@gmail.com>
2025-10-29 23:03:14 -0300

    listo

--- hoteles/seed.js
+++ hoteles/seed.js
@@ -18,6 +18,7 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || 'root',
+  password: process.env.DB_PASS || 'Jhoselin712.',
   database: process.env.DB_NAME || 'hotelesBA'
 });
 
